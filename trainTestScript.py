44a9d7eab124c044a0ba0847041a4041204e2b98
albarosaali <72601330+albarosaali@users.noreply.github.com>
2021-04-23 18:46:29 +0100

    Add comments

--- trainTestScript.py
+++ trainTestScript.py
@@ -1,12 +1,11 @@
 import pitchDetectionNetTraining as trainFunc
 
+# Choose folder containing desired dataset
+# and type in the name of the dataset
 folder = 'TrainingDatasets/Guitar_Trump/'
 dataset = 'GTSetF2048_H512_Take_3_Size_18722_4AscOnly.h5'
+# Get training data and labels into RAM
 train, labels = trainFunc.get_2_inst_data(folder, dataset)
-trainFunc.log5KFoldTest_two_inst(trainFunc.shallow_2_inst_model_both_tallThin_8feat(), 'shallow_2_inst_model_both_tallThin_8feat', train, labels, dataset, epochs = 20)  
-## Not working atm 
-'''
-dataset = 'GTSetF1024_H512_Take_3_Size_37445_4AscOnly.h5'
-train, labels = trainFunc.get_2_inst_data(folder, dataset)
-trainFunc.log5KFoldTest_two_inst(trainFunc.shallow_2_inst_model(), 'shallow_2_inst_model', train, labels, dataset, epochs = 20) '''
+# Run K=5-Fold test, (model, saveFileName, train, labels, dataset, epochs)
+trainFunc.log5KFoldTest_two_inst(trainFunc.shallow_2_inst_model_both_tallThin_8feat(), 'shallow_2_inst_model_both_tallThin_8feat', train, labels, dataset, epochs = 20)
 
